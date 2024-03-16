7c16502854e64cfdf589aebc0f9d104f66891cef
yinhao <yinhao@5eplay.com>
2024-03-16 09:00:49 +0800

    poll

--- select.cpp
+++ select.cpp
@@ -1,6 +1,7 @@
 #include "select.hpp"
 #include <algorithm>
-cc::Select::Select() {
+cc::Select::Select()
+{
     FD_ZERO(&m_readfd);
     FD_ZERO(&m_writefd);
     FD_ZERO(&m_errorfd);
@@ -8,16 +9,18 @@ cc::Select::Select() {
 
 cc::Select::~Select() {}
 
-void cc::Select::add(int fd,cc::Select::Config config)
+void cc::Select::add(int fd, cc::Select::Config config)
 {
     maxfd = std::max(maxfd, fd);
-    if(config & SelectRead){
+    if (config & SelectRead)
+    {
         FD_SET(fd, &m_readfd);
     }
-    if(config & SelectWrite){
+    if (config & SelectWrite)
+    {
         FD_SET(fd, &m_writefd);
     }
-     FD_SET(fd, &m_errorfd);
+    FD_SET(fd, &m_errorfd);
     m_fds.push_back(fd);
 }
 
@@ -29,15 +32,15 @@ int cc::Select::wait(TimeInterval timeout)
 {
     int sec = floor(timeout);
     int nano = (timeout - sec) * 1000 * 1000 * 1000;
-    timeval m {sec,nano};
+    timeval m{sec, nano};
     return cc::Select::wait(m);
 }
 
 void cc::Select::occur_write(std::vector<int> &fd)
 {
-    for (auto i =  m_fds.begin() ; i < m_fds.end(); i++)
+    for (auto i = m_fds.begin(); i < m_fds.end(); i++)
     {
-        if (FD_ISSET(*i,&m_writefd))
+        if (FD_ISSET(*i, &m_writefd))
         {
             fd.push_back(*i);
         }
@@ -45,9 +48,9 @@ void cc::Select::occur_write(std::vector<int> &fd)
 }
 void cc::Select::occur_read(std::vector<int> &fd)
 {
-    for (auto i =  m_fds.begin() ; i < m_fds.end(); i++)
+    for (auto i = m_fds.begin(); i < m_fds.end(); i++)
     {
-        if (FD_ISSET(*i,&m_readfd))
+        if (FD_ISSET(*i, &m_readfd))
         {
             fd.push_back(*i);
         }
@@ -55,11 +58,45 @@ void cc::Select::occur_read(std::vector<int> &fd)
 }
 void cc::Select::occur_error(std::vector<int> &fd)
 {
-    for (auto i =  m_fds.begin() ; i < m_fds.end(); i++)
+    
+    for (auto i = m_fds.begin(); i < m_fds.end(); i++)
     {
-        if (FD_ISSET(*i,&m_errorfd))
+        if (FD_ISSET(*i, &m_errorfd))
         {
             fd.push_back(*i);
         }
     }
+}
+
+cc::Poll::Poll()
+{
+}
+cc::Poll::~Poll()
+{
+}
+void cc::Poll::add(int fd, Config config)
+{
+    pollfd p_fd;
+    p_fd.fd = fd;
+    p_fd.events = config;
+    p_fd.revents = 0;
+    m_poll_fd.push_back(p_fd);    
+}
+
+int cc::Poll::wait(TimeInterval seconds)
+{
+    return poll(m_poll_fd.data(),m_poll_fd.size(),seconds * 1000);
+}
+void cc::Poll::occur(std::vector<PollResult> &result){
+    for (auto i = m_poll_fd.begin(); i < m_poll_fd.end(); i++)
+    {
+        if(i->revents != 0){
+            PollResult pr;
+            pr.fd = i->fd;
+            pr.config = (Config)i->revents;
+            result.push_back(pr);
+            i->revents = 0
+        }
+    }
+    
 }
--- select.hpp
+++ select.hpp
@@ -1,10 +1,9 @@
 #ifndef SELECT_HPP
-#define  SELECT_HPP
+#define SELECT_HPP
 
 #include <sys/select.h>
 #include <sys/poll.h>
 #include <vector>
-
 
 #include "time.hpp"
 
@@ -35,6 +34,37 @@ namespace cc
         int maxfd = 0;
         std::vector<int> m_fds;
         fd_set m_readfd, m_writefd, m_errorfd;
+    };
+
+    class Poll
+    {
+    public:
+        enum Config{
+            ConfigIN          = 0x0001,           /* any readable data available */
+            ConfigPRI         = 0x0002,           /* OOB/Urgent readable data */
+            ConfigOUT         = 0x0004,           /* file descriptor is writeable */
+            ConfigRDNORM      = 0x0040,           /* non-OOB/URG data available */
+            ConfigWRNORM      = ConfigOUT,        /* no write type differentiation */
+            ConfigRDBAND      = 0x0080,           /* OOB/Urgent readable data */
+            ConfigWRBAND      = 0x0100,           /* OOB/Urgent data can be written */
+            ConfigERR         = 0x0008,           /* some poll error occurred */
+            ConfigHUP         = 0x0010,           /* file descriptor was "hung up" */
+            ConfigNVAL        = 0x0020,           /* requested events "invalid" */
+        };
+        struct PollResult
+        {
+            int fd;
+            Config config;
+        };
+    public:
+        Poll();
+        ~Poll();
+        void    add(int fd,Config config);
+        int     wait(TimeInterval seconds);
+        void    occur(std::vector<PollResult> &result);
+    private:    
+        std::vector<pollfd> m_poll_fd;
+        
     };
 
 } // namespace cc
